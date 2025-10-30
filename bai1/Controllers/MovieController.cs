819e2dcb094c41469cccf0afc3a25330ed8f577a
Admin <Admin@DESKTOP-HL25HJ5>
2025-10-30 09:54:36 +0700

    Add project files.

---
+++ bai1/Controllers/MovieController.cs
@@ -0,0 +1,29 @@
+﻿using bai1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bai1.Controllers
+{
+    public class MovieController : Controller
+    {
+        //new () { Id = 1, Title = " Fish & Cat",
+        //        Description = " A fish and a cat are best friends, ", Rate = 5 },
+        //    new () { Id = 2, Title = " Dog & Goat",
+        //        Description = " A Dog and a Goat aren't best friends, ", Rate = 3
+        //    },
+        //    new()
+        //    {
+        //        Id = 3,
+        //        Title = " Fast & Furios",
+        //        Description = " Fast & FUrios are the best movie, ",
+        //        Rate = 5
+        //    },
+        public List<Movie> listMovies = new List<Movie>() {
+            
+        };
+
+        public IActionResult Index()
+        {
+            return View(listMovies);
+        }
+    }
+}
---
+++ bai1/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace bai1.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Rate { get; set; }
+    }
+}
