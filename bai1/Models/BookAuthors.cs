0123ba37c67f0d259699412d88fa0a20feb9bb9f
Mrrooy08 <nguyenledat812@gmail.com>
2025-11-17 23:59:57 +0700

    update

--- bai1/Models/BookAuthors.cs
+++ bai1/Models/BookAuthors.cs
@@ -8,6 +8,7 @@
         public int AuthorId { get; set; }
         public Person Author { get; set; }
 
+        //Cần xóa vì k có tác giả chính mà chỉ là đồng tác giả
         public bool IsMainAuthor { get; set; }
 
     }
---
+++ bai1/Models/Dto/BookAuthorsViewModels.cs
@@ -0,0 +1,10 @@
+﻿namespace bai1.Models.Dto
+{
+    public class BookAuthorsViewModels
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+
+
+    }
+}
